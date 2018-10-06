6c68941a52be0e065dc4b58adbc417d0715e7bab
yeeegem <yeeegem@users.noreply.github.com>
2018-10-07 01:33:56 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, render_template
 application = Flask(__name__, static_url_path='')
 
 @application.route("/")
 def hello():
-    return application.send_static_file('index.html')
+    ##return application.send_static_file('index.html')
+    return render_template('index.html', flask_debug=application.debug)
 
 if __name__ == "__main__":
     application.run(debug=True)
